78a0ed662843935d4099cdd9d6ae497dd16f6a04
moyersd28-cyber <moyersd28@gmail.com>
2026-06-18 10:41:03 -0400

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,68 @@
+// =====================================================
+// EmpireCAD v2
+// Firebase Configuration
+// =====================================================
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+
+import {
+    getAuth,
+    onAuthStateChanged,
+    signInWithEmailAndPassword,
+    signOut,
+    createUserWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+
+import {
+    getFirestore,
+    collection,
+    addDoc,
+    setDoc,
+    getDoc,
+    getDocs,
+    updateDoc,
+    deleteDoc,
+    doc,
+    serverTimestamp,
+    onSnapshot,
+    query,
+    where,
+    orderBy,
+    limit
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBfYHAUU4yrbYqZGGbps_ahQjbX16EtW5g",
+    authDomain: "nycrp911.firebaseapp.com",
+    projectId: "nycrp911",
+    storageBucket: "nycrp911.firebasestorage.app",
+    messagingSenderId: "291055295348",
+    appId: "1:291055295348:web:cd0981f30dab930baaaa21"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+export const db = getFirestore(app);
+
+export {
+    collection,
+    addDoc,
+    setDoc,
+    getDoc,
+    getDocs,
+    updateDoc,
+    deleteDoc,
+    doc,
+    serverTimestamp,
+    onSnapshot,
+    query,
+    where,
+    orderBy,
+    limit,
+    onAuthStateChanged,
+    signInWithEmailAndPassword,
+    signOut,
+    createUserWithEmailAndPassword
+};
